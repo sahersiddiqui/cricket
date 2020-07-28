7b255e199b40fef9b2ea6ce6f8fe23d468b1a184
najmussaher <najmussaher>
2020-07-28 21:33:22 +0530

    changes for layout

---
+++ resources/views/admin/teams/add.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.layouts.master')
+@push('css')
+
+
+@endpush
+@section("content")
+<!-- Page Heading -->
+<h1 class="h3 mb-2 text-gray-800">Team</h1>
+
+
+@endsection
+
+@push('js')
+@endpush
