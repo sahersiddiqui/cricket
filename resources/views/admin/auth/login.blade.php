0dcde04d438b3a0506c4b3871a40f2959ccdc31b
najmussaher <najmussaher>
2020-08-05 11:04:07 +0530

    changes for css

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -13,7 +13,7 @@
     <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- Custom styles for this template-->
-    <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/admin.min.css')}}" rel="stylesheet">
 </head>
 
 <body class="bg-gradient-primary">
@@ -83,7 +83,7 @@
     <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
     
     <!-- Custom scripts for all pages-->
-    <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+    <script src="{{asset('js/admin.min.js')}}"></script>
     
 </body>
 
